f8ac8610d581685cf5ac19bf983cc2b4adbb48dc
Fillipe143 <fillipedev.contato@gmail.com>
2024-07-06 09:42:23 -0300

    added validation for file path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 macro_rules! throw_error {
     ($($arg: tt)*) => {
         println!($($arg)*);
@@ -6,13 +8,24 @@ macro_rules! throw_error {
 }
 
 fn read_arg(args: &mut Vec<String>, msg: &str) -> String {
-    if args.len() == 0 { throw_error!("cmd line error: {}", msg); }
+    if args.len() == 0 { throw_error!("Command line error: {}", msg); }
     args.pop().unwrap()
+}
+
+fn check_file_path(file_path: &str) {
+    if !file_path.ends_with(".bf") { throw_error!("Invalid file extension"); }
+
+    let file_metadata = fs::metadata(file_path);
+    if file_metadata.is_err() { throw_error!("No such file '{}'", file_path); }
+
+    let file_type = file_metadata.unwrap().file_type();
+    if !file_type.is_file() { throw_error!("This path is not to a file"); }
 }
 
 fn main() {
     let mut args: Vec<String> = std::env::args().rev().collect();
 
-    let _ = read_arg(&mut args, "unable to read program arguments");
-    let _file_path = read_arg(&mut args, "file path not given");
+    let _ = read_arg(&mut args, "Unable to read program arguments");
+    let file_path = read_arg(&mut args, "File path not given");
+    check_file_path(file_path.as_str());
 }
