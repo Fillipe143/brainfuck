423e76752b0002d564f83c120435a23e449e0796
Fillipe143 <fillipedev.contato@gmail.com>
2024-07-08 11:59:35 -0300

    added function to extract and group all operators

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,48 @@ fn read_file(file_path: &str) -> Vec<u8> {
     else { throw_error!("Unable to read file '{}'", file_path); }
 }
 
+type Operator = (char, usize);
+
+fn extract_operators(data: &mut Vec<u8>) -> Vec<Operator> {
+    let mut operators = Vec::<Operator>::new();
+    let mut open_brackets = Vec::<usize>::new();
+
+    for byte in data.iter() {
+        let curr_char = *byte as char;
+
+        match curr_char {
+            '<' | '>' | '+' | '-' | '.' | ',' => {
+                if let Some(last_operator) = operators.last_mut() {
+                    if last_operator.0 == curr_char { last_operator.1 += 1; }
+                    else { operators.push((curr_char, 1)); }
+                } else { operators.push((curr_char, 1)); }
+            },
+            '[' => {
+                open_brackets.push(operators.len());
+                operators.push((curr_char, 0));
+            },
+            ']' => {
+                if open_brackets.is_empty() { throw_error!("Unbalanced brackets"); }
+                let opening_index = open_brackets.pop().unwrap();
+
+                operators[opening_index].1 = operators.len();
+                operators.push((curr_char, opening_index));
+            },
+            _ => {}
+        }
+    }
+
+    if !open_brackets.is_empty() { throw_error!("Unbalanced brackets"); }
+    operators
+}
+
 fn main() {
     let mut args: Vec<String> = std::env::args().rev().collect();
 
     let _ = read_arg(&mut args, "Unable to read program arguments");
     let file_path = read_arg(&mut args, "File path not given");
-    let data = read_file(file_path.as_str());
-    println!("{:?}", data);
+
+    let mut data = read_file(file_path.as_str());
+    let operators = extract_operators(&mut data);
+    println!("{:?}", operators);
 }
